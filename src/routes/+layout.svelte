12ce775ffb9aa2f9453ea020b87f07963d566d5d
bliu <brianliu03@gmail.com>
2023-07-25 21:08:53 -0700

    Continue work on appbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -23,24 +23,49 @@ function scrollHandler(event: ComponentEvents<AppShell>['scroll']) {
 
 
 <AppShell ... on:scroll={scrollHandler}> 
+
 	<!-- (header) -->
 	<!-- (sidebarLeft) -->
 	<!-- (sidebarRight) -->
+
 	<svelte:fragment slot="pageHeader">
     <AppShell regionPage="relative" slotPageHeader="sticky top-0 z-10">
       <svelte:fragment slot="pageHeader">
-        
-        <AppBar>
-          <svelte:fragment slot="lead">Title</svelte:fragment>
-          <svelte:fragment slot="trail">Trail</svelte:fragment>
+        <AppBar background="bg-surface-50-900-token" border="border-b-2 bg-primary-50" padding="px-10 py-4">
+          <svelte:fragment slot="lead">
+            <a class="w-20" href="/" title="Go to top">
+              Brian Liu
+            </a>
+          </svelte:fragment>
+          <svelte:fragment slot="trail">
+            <!-- below but now with about, projects, github, linkedin-->
+            <div class="flex items-center space-x-4">
+              <a href="/about" title="About">
+                About
+              </a>
+              <a href="/projects" title="Projects">
+                Projects
+              </a>
+              <a href="https://github.com/brianliu03" title="Github">
+                Github
+              </a>
+              <a class="btn-icon btn-icon-sm hover:variant-soft-primary" href="https://discord.gg/EXqV7W8MtY" target="_blank" rel="noreferrer">
+                <i class="fa-brands fa-discord" />
+              </a>
+              <a href="https://www.linkedin.com/in/brianliu03/" title="LinkedIn">
+                LinkedIn
+              </a>
+          </svelte:fragment>
         </AppBar>
-
       </svelte:fragment>
       <!-- ... -->
     </AppShell>
   </svelte:fragment>
+
 	<!-- Router Slot -->
-	<slot />
+	<div class="container p-10 mx-auto">
+    <slot />
+  </div>
 	<!-- ---- / ---- -->
 	<svelte:fragment slot="pageFooter">Page Footer</svelte:fragment>
 	<!-- (footer) -->
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,8 @@
-<!-- Home page
-<script>
-  import Header from '$lib/components/Header.svelte';
+<script lang='ts'>
+  
 </script>
-
-<Header />
 
 <main class="container mx-auto my-8">
   <h1 class="text-4xl font-bold mb-4">Welcome! I'm Brian - A California Native Interested in Technology for Human Good</h1>
   <h2 class="text-2xl font-bold mb-4">TODO</h2>
-</main> -->
+</main>

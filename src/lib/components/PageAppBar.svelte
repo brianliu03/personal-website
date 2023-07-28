16fc244927adb8502260515cbcd5c2e73dcc1dbd
bliu <brianliu03@gmail.com>
2023-07-27 22:33:21 -0700

    Extract Appbar component

---
+++ src/lib/components/PageAppBar.svelte
@@ -0,0 +1,46 @@
+<script lang='ts'>
+  
+  import {
+    AppBar,
+    AppShell
+  } from '@skeletonlabs/skeleton';
+
+
+import type { ComponentEvents } from 'svelte';
+
+function scrollHandler(event: ComponentEvents<AppShell>['scroll']) {
+	console.log(event.currentTarget.scrollTop);
+}
+
+  
+</script>
+
+<AppShell regionPage="relative" slotPageHeader="sticky top-0 z-10">
+      <svelte:fragment slot="pageHeader">
+        <AppBar background="bg-surface-50-900-token" border="border-b-2 bg-primary-50" padding="px-10 py-4">
+          <svelte:fragment slot="lead">
+            <a class="w-20" href="/" title="Go to top">
+              Brian Liu
+            </a>
+          </svelte:fragment>
+          <svelte:fragment slot="trail">
+            <!-- below but now with about, projects, github, linkedin-->
+            <div class="flex items-center space-x-4">
+              <a href="/about" title="About">
+                About
+              </a>
+              <a href="/projects" title="Projects">
+                Projects
+              </a>
+              <a class="btn-icon btn-icon-sm hover:variant-soft-primary" href="https://github.com/brianliu03" title="Github" target="_blank">
+                <i class="fa-brands fa-github text-lg" />
+              </a>
+              <a class="btn-icon btn-icon-sm hover:variant-soft-primary" href="https://www.linkedin.com/in/brianliu03/" title="LinkedIn" target="_blank">
+                <i class="fa-brands fa-linkedin text-lg" />
+              </a>
+          </svelte:fragment>
+        </AppBar>
+      </svelte:fragment>
+      <!-- ... -->
+    </AppShell>
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,6 @@
 </script>
 
 <main class="container mx-auto my-8">
-  <h1 class="text-4xl font-bold mb-4">Welcome! I'm Brian - A California Native Interested in Technology for Human Good</h1>
-  <h2 class="text-2xl font-bold mb-4">TODO</h2>
+  <h1 class="h1">Welcome! I'm Brian - A California Native Interested in Technology for Human Good</h1>
+  <h2 class="h2">TODO</h2>
 </main>

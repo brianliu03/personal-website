4a21ccc67e8f3f48f0ad98e556fc1ee252a380d6
bliu <brianliu03@gmail.com>
2023-07-28 18:31:41 -0700

    Add paragraph about me section

---
+++ src/lib/components/About.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let title = "About me";
+</script>
+
+<div class="flex flex-col items-start">
+  <h1 class="text-3xl font-bold">{title}</h1>
+  <hr class="w-16 border-2 border-primary-500 my-2">
+  <p class="h4">
+    Placeholder text placeholder text placeholder text placeholder text placeholder text placeholder text placeholder text placeholder text placeholder text placeholder text placeholder text placeholder text placeholder text placeholder text placeholder text placeholder text placeholder text.
+  </p>
+  <p class="h4 mt-4">
+    Explore my work <a href="/work" class="text-primary-500">here</a>.
+  </p>
+</div>
+ ▌

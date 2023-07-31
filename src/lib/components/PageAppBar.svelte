21a854b69812915cc4b1d4f4fb37c5ca3bb2fde8
bliu <brianliu03@gmail.com>
2023-07-30 17:05:11 -0700

    UI changes to appbar

--- src/lib/components/PageAppBar.svelte
+++ src/lib/components/PageAppBar.svelte
@@ -9,16 +9,16 @@
       <svelte:fragment slot="pageHeader">
         <AppBar background="bg-surface-50-900-token" border="border-b-2 bg-primary-50" padding="px-10 py-4">
           <svelte:fragment slot="lead">
-            <a class="w-20" href="/" title="Go to top">
+            <a class="h4 font-semibold" href="/" title="Go to top">
               Brian Liu
             </a>
           </svelte:fragment>
           <svelte:fragment slot="trail">
-            <div class="flex items-center space-x-4">
-              <a href="/#about" title="About">
+            <div class="flex items-center space-x-4 text-secondary-300">
+              <a class="btn hover:variant-soft-primary" href="/#about" title="About">
                 About
               </a>
-              <a href="/projects" title="Projects">
+              <a class="btn hover:variant-soft-primary" href="/projects" title="Projects">
                 Projects
               </a>
               <a class="btn-icon btn-icon-sm hover:variant-soft-primary" href="https://github.com/brianliu03" title="Github" target="_blank">

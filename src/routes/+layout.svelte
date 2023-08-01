282b951b8c436fa632af33d5c1a6a86bc7bf358b
bliu <brianliu03@gmail.com>
2023-07-31 17:43:53 -0700

    Add custom theme

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,13 @@
 <script lang='ts'>
   import PageAppBar from '$lib/components/PageAppBar.svelte';
 
-	// The ordering of these imports is critical to your app working properly
-	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
-	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
-	import '@skeletonlabs/skeleton/styles/skeleton.css';
-	// Most of your app wide CSS should be put in this file
-	import '../app.postcss';
+
+// Your custom Skeleton theme:
+import '../theme.postcss';
+// This contains the bulk of Skeletons required styles:
+import '@skeletonlabs/skeleton/styles/skeleton.css';
+// Finally, your application's global stylesheet (sometimes labeled 'app.css')
+import '../app.postcss';
 
   import {
     AppBar,

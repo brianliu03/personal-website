ec8e783a67ccb464c30ec4e23a5a3e3f7387e47f
bliu <brianliu03@gmail.com>
2023-08-07 16:33:16 -0700

    Add About

--- src/lib/components/About.svelte
+++ src/lib/components/About.svelte
@@ -1,13 +1,31 @@
 <script>
   export let title = "About me";
-  export let about = "I am a native of Irvine, California, a student at Bowdoin College, and a visiting student at the University of Oxford. I am currently pursuing a Bachelor of Science in Computer Science and a Bachelor of Arts in Philosophy. I am interested in the intersection of technology and the human good. I believe that technology should be used to improve the lives of people and that it should be used to solve the world's most pressing problems. I am also interested in the philosophy of technology and the ethics of technology. I am currently working on a project that explores the ethics of artificial intelligence and the ethics of autonomous vehicles. I am also interested in the philosophy of mind and the philosophy of language. I am currently working on a project that explores the relationship between language and thought. I am also interested in the philosophy of science and the philosophy of mathematics. I am currently working on a project that explores the nature of scientific theories and the nature of mathematical theories.";
+  export let intro = "Hello! I'm a student interested in utilizing technology for human good, addressing pressing humanitarian issues, and creating empowering software for all. Outside work, the current housing and climate crises have captured my attention, and I am exploring statistical and computational models to not only chronicle historical events, but also to help uncover potential root causes and solutions. Presently, I am currently looking for software and data roles for the summer of 2024.";
+  export let subCuriousity = "My fascination with data-driven insights led me to play with models and analyze intricate datasets. I recently delved into a research project focused on studying evictions in San Francisco and their correlation with financial data. Unraveling meaningful patterns and uncovering hidden truths from the numbers was both challenging and deeply rewarding.";
+  export let subDesign = "During my internship, I had the opportunity to create a full-stack chat application for an airline, a project that allowed me to blend my technical skills with a genuine desire to improve user experiences. This experience ignited my passion for building applications that directly impact people's lives positively.";
+  export let subConnect = "I find immense fulfillment in fostering human connections and prioritizing effective communication. As an algorithms learning assistant during my time at Bowdoin College, I not only enjoyed helping others navigate the intricacies of the subject but also cherished the shared sense of growth and understanding.";
+  export let subExtra = "My lifelong commitment to piano and music has been a source of creativity and joy. I've had the privilege of performing and receiving top prizes in competitions, and I continue to dedicate time everyday for music. As a challenge, I am preparing repertoire for international competitions. I also love the outdoors, and running and mountain biking are two ways I explore nearby mountains and forests.";
+  export let bye = "Thank you for stopping by, feel free to connect with me!";
 </script>
 
-<div class="flex flex-col items-center px-20 bg-secondary-300 text-surface-500" id="about">
+<div class="flex flex-col items-center px-20 bg-secondary-200 text-surface-700" id="about">
   <h1 class="h2 font-semibold mt-20">{title}</h1>
   <hr class="w-3/4 border-2 border-primary-500 mt-3">
-  <p class="h4 my-10">{about}</p>
-  <p class="h4 mt-4">
-    Explore my work <a href="/work" class="text-primary-500">here</a>.
-  </p>
+  <p class="h5 mx-15 my-5">{intro}</p>
+  <!-- put a div below that splits page vertically into two, so first column has subCuriousity and below it subConnect, and right column has subDesign and subExtra -->
+  <div class="flex flex-row justify-center items-center w-full">
+    <div class="flex flex-col justify-center items-center w-1/2">
+      <h2 class="h3 font-semibold mt-10">Curiosity and Exploring Data</h2>
+      <p class="h5 mx-15 my-5">{subCuriousity}</p>
+      <h2 class="h3 font-semibold mt-10">Communication and Connections</h2>
+      <p class="h5 mx-15 my-5">{subConnect}</p>
+    </div>
+    <div class="flex flex-col justify-center items-center w-1/2">
+      <h2 class="h3 font-semibold mt-10">Designing, Building, and Testing</h2>
+      <p class="h5 mx-15 my-5">{subDesign}</p>
+      <h2 class="h3 font-semibold mt-10">Away from Computers</h2>
+      <p class="h5 mx-15 my-5">{subExtra}</p>
+    </div>
+  </div>
+  <p class="h5 mx-15 my-5">{bye}</p>
 </div>

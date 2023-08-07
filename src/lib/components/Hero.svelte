4206cbae9e0cea9cf9534320d901e7b4b2ad48f7
bliu <brianliu03@gmail.com>
2023-08-06 18:57:24 -0700

    Add chevron

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -14,20 +14,27 @@
   });
 </script>
 
-<div class="flex items-center mx-20 space-x-20 h-screen">
-  <div class="flex-1">
+<div class="flex-row h-4/5">
+  <div class="flex items-center mx-20 py-40 space-x-20">
+    <div class="flex-1">
+      {#if visible}
+        <h1 class="h2 mb-4" transition:fly="{{ y: 20, duration: 500 }}">
+          {title}
+        </h1>
+      {/if}
+      {#if visible}
+        <h2 class="h4" transition:fly="{{ y: 250, duration: 1000, delay: 500 }}">
+          {subtitle}
+        </h2>
+      {/if}
+    </div>
     {#if visible}
-      <h1 class="h2 mb-4" transition:fly="{{ y: 20, duration: 500 }}">
-        {title}
-      </h1>
-    {/if}
-    {#if visible}
-      <h2 class="h4" transition:fly="{{ y: 250, duration: 1000, delay: 500 }}">
-        {subtitle}
-      </h2>
+      <img src={imageSrc} class="w-48 h-48 object-cover rounded-3xl" alt="Avatar" transition:fly="{{ y: -250, duration: 1000, delay: 1250 }}"/>
     {/if}
   </div>
-  {#if visible}
-    <img src={imageSrc} class="w-48 h-48 object-cover rounded-3xl" alt="Avatar" transition:fly="{{ y: -250, duration: 1000, delay: 1250 }}"/>
-  {/if}
+  <div class="flex justify-center">
+    <a class="btn hover:variant-soft-primary flex-col" href="/#about" style="cursor: pointer;" transition:fade="{{duration: 1000}}">
+      <i class="fa-solid fa-chevron-down text-2xl" />
+    </a>
+  </div>
 </div>

5942d24b6a44643bcd25b6e29e102e9b23e45cfe
bliu <brianliu03@gmail.com>
2023-08-16 21:06:42 -0700

    Bug fix for dropdown

--- src/lib/components/PageAppBar.svelte
+++ src/lib/components/PageAppBar.svelte
@@ -6,60 +6,55 @@
   } from '@skeletonlabs/skeleton';
 </script>
 
-<AppShell regionPage="relative" slotPageHeader="sticky top-0 z-10">
-      <svelte:fragment slot="pageHeader">
-        <AppBar background="bg-surface-700" border="border-b-2 bg-primary-50" padding="px-10 py-4">
-          <svelte:fragment slot="lead">
-            <a class="h4 font-semibold" href="/" title="Go to top">
-              Brian Liu
-            </a>
-          </svelte:fragment>
-          <svelte:fragment slot="trail">
-            <div class="flex items-center space-x-1 text-primary-300">
-              <a class="btn hover:variant-soft-primary" href="/#about" title="About">
-                About
+
+<AppBar background="bg-surface-700" border="border-b-2 bg-primary-50" padding="px-10 py-4">
+  <svelte:fragment slot="lead">
+    <a class="h4 font-semibold" href="/" title="Go to top">
+      Brian Liu
+    </a>
+  </svelte:fragment>
+  <svelte:fragment slot="trail">
+    <div class="flex items-center space-x-1 text-primary-300">
+      <a class="btn hover:variant-soft-primary" href="/#about" title="About">
+        About
+      </a>
+      <!-- Projects dropdown -->
+      <button class="btn hover:variant-soft-primary" use:popup={{ event: 'click', target: 'features'}}>
+        <span>Projects</span>
+        <i class="fa-solid fa-caret-down opacity-50" />
+      </button>
+      <!-- Projects pop up -->
+      <div class="card w-60" data-popup="features">
+        <nav class="list-nav">
+          <ul>
+            <li>
+              <a href="/projects" title="Housing SF">
+                Housing SF
               </a>
-              <!-- Projects dropdown -->
-              <button class="btn hover:variant-soft-primary" use:popup={{ event: 'click', target: 'features'}}>
-                <span>Projects</span>
-                <i class="fa-solid fa-caret-down opacity-50" />
-              </button>
-              <!-- Projects pop up -->
-              <div class="card variant-glass-tertiary p-4 w-60" data-popup="features">
-                arstarstarsstarst
-                <nav class="list-nav">
-                  <ul>
-                    <li>
-                      <a href="/projects" title="Housing SF">
-                        Housing SF
-                      </a>
-                    </li>
-                    <li>
-                      <a class="nav-link" href="/music" title="FractMuse">
-                        FractMuse
-                      </a>
-                    </li>
-                  </ul>
-                </nav>
-                <div class="arrow bg-primary-500" />
-              </div>
-              <a class="btn hover:variant-soft-primary" href="/projects" title="Projects">
-                Projects
+            </li>
+            <li>
+              <a class="nav-link" href="/music" title="FractMuse">
+                FractMuse
               </a>
-              <a class="btn hover:variant-soft-primary" href="/music" title="Music">
-                Music
-              </a>
-            </div>
-            <div class="flex items-center space-x-6 text-primary-300">
-              <a class="btn-icon btn-icon-sm hover:variant-soft-primary" href="https://github.com/brianliu03" title="Github" target="_blank">
-                <i class="fa-brands fa-github text-lg" />
-              </a>
-              <a class="btn-icon btn-icon-sm hover:variant-soft-primary" href="https://www.linkedin.com/in/brianliu03/" title="LinkedIn" target="_blank">
-                <i class="fa-brands fa-linkedin text-lg" />
-              </a>
-            </div>
-          </svelte:fragment>
-        </AppBar>
-      </svelte:fragment>
-      <!-- ... -->
-    </AppShell>
+            </li>
+          </ul>
+        </nav>
+        <div class="arrow bg-primary-500" />
+      </div>
+      <a class="btn hover:variant-soft-primary" href="/projects" title="Projects">
+        Projects
+      </a>
+      <a class="btn hover:variant-soft-primary" href="/music" title="Music">
+        Music
+      </a>
+    </div>
+    <div class="flex items-center space-x-6 text-primary-300">
+      <a class="btn-icon btn-icon-sm hover:variant-soft-primary" href="https://github.com/brianliu03" title="Github" target="_blank">
+        <i class="fa-brands fa-github text-lg" />
+      </a>
+      <a class="btn-icon btn-icon-sm hover:variant-soft-primary" href="https://www.linkedin.com/in/brianliu03/" title="LinkedIn" target="_blank">
+        <i class="fa-brands fa-linkedin text-lg" />
+      </a>
+    </div>
+  </svelte:fragment>
+</AppBar>

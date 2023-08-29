f630945c61fcfbf6b6cd0a7a4cc7e96eae8d5241
bliu <brianliu03@gmail.com>
2023-08-28 22:08:36 -0700

    Finish basic housing page

--- src/routes/fractmuse/+page.svelte
+++ src/routes/fractmuse/+page.svelte
@@ -4,6 +4,7 @@
     import type { PageData } from './$types';
 
     export let title = "FractMuse: Discovering new compositional workflows";
+    export let subtitle = "Music and mathematics, despite differences in their expressions, are tied together. The concept of scales and compositional techniques are often derived from mathematical transformations, and many mathematicians and musicians are known for intersecting work in the other respective field. With the increase of technology in everything we do, I have become interested in how technology can empower humans in their creative works, and FractMuse showcases my different explorations in algorithmic and machine learning based composition.";
     export let refPoster = "https://drive.google.com/file/d/1sDyDam0oKtqYpLp__ofFcDvPNpnso7Sd/view?usp=sharing";
 
 
--- src/routes/housing/+page.svelte
+++ src/routes/housing/+page.svelte
@@ -5,12 +5,12 @@
   
   export let title = "Crossroads of a Crisis: Housing in San Francisco";
   export let imgGroup = "/images/groupsf.JPG";
-  export let about = "Over the course of Summer and Fall 2022, my friend and I were sponsored by the McKeen Center in Bowdoin to create a holistic, seminar-based learning experience on the housing crisis next Spring. We brought on ten other students, held seven weekly seminars, and traveled to San Francisco for a week to work with community partners supporting unhoused people. You can view some of our material used here.";
+  export let about = "Over the course of Summer and Fall 2022, my friend and I were sponsored by the McKeen Center in Bowdoin to create a holistic, seminar-based learning experience on the housing crisis next Spring. We brought on ten other students, held seven weekly seminars, and traveled to San Francisco for a week to work with community partners supporting unhoused people. You can view some of our material used here:";
   export let refLessonPlan = "https://drive.google.com/file/d/1PLjTq_dkpDOC80AwlggnveaQC25Tux-_/view?usp=sharing";
   export let titStats = "Eviction Notices and Data Analysis";
   export let gifEvictions = "/images/evictionsf.gif";
   export let aboutStats = "After the trip, I became interested in exploring eviction data and decided to start a statistics project to help explain historical events and how landlords can play a “game” of eviction strategies based on the housing market. I presented my first analysis and findings in May 2023, and am currently diving deeper into modeling and incorporating financial data.";
-  export let subStats = "I am working on publishing my findings and continuing work, but here you can find my past presentation and analysis."
+  export let refGithub = "https://github.com/brianliu03/sf-evictions";
 
   let visible = false;
 
@@ -22,13 +22,17 @@
 
 <div class="flex flex-col items-center px-20" id="about">
 
-  <div class="flex flex-col justify-between w-full space-y-20">
+  <div class="flex flex-col justify-between items-center w-full space-y-20">
 
     <div class="flex flex-row w-full mt-10 space-x-10">
       <div class="flex flex-col w-1/2">
         <h1 class="h2 font-semibold">{title}</h1>
         <hr class="w-full !border-2 border-primary-500 mt-3">
-        <p class="h5 mx-15 my-5">{about}</p>
+        <p class="h5 mx-15 my-5">{about}
+          <a class="text-primary-500" href={refLessonPlan} target="_blank">
+            <h3 class="font-semibold inline-block">Lesson Plan</h3>
+          </a>
+        </p>
       </div>
       <img src={imgGroup} class="w-1/2 object-scale-down" alt="Avatar" />
     </div>
@@ -40,6 +44,10 @@
         <p class="h5 mx-15 my-5">{aboutStats}</p>
       </div>
     </div>
-    <p class="h5 mx-15 my-5">{subStats}</p>
+
+    <a class="text-primary-500" href={refGithub} target="_blank">
+      <h3 class="h4 font-semibold mb-20">Github Repository</h3>
+    </a>
+
   </div>
 </div>

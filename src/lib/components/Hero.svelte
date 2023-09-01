539489ede76938a000a74472a2148b541e56c077
bliu <brianliu03@gmail.com>
2023-09-01 10:59:42 -0700

    Reorganize hero

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -3,8 +3,10 @@
   import { onMount } from "svelte";
 
   export let title = "Welcome, my name is Brian.";
-  export let subtitle = "I am a visiting student at the University of Oxford and a student of Bowdoin College.";
+  export let subtitle = "I am a visiting student at the University of Oxford and a student at Bowdoin College.";
   export let imageSrc = "/images/hero_portrait.jpg";
+  export let svgHello = "/svg/hello.svg";
+  export let about = "Hello! I am interested in utilizing technology for human good, addressing pressing humanitarian issues, and creating empowering software for all. The current housing crisis has captured my attention, and I am exploring statistical and computational models to chronicle historical events and find relationships that explain variation. I am also interested in the intersection of math, music, and creativity. This includes human computer interaction, stochastic processes, and machine learning.";
 
   let visible = false;
 
@@ -14,24 +16,35 @@
   });
 </script>
 
-<div class="flex flex-col m-10">
-  <div class="flex items-center justify-center space-x-64">
+<div class="flex flex-col">
+  <div class="flex items-center justify-center space-x-16 mx-5 md:mx-10 md:space-x-32">
     <div class="flex flex-col">
       {#if visible}
-        <h1 class="h2 mb-10" in:fly="{{ y: 20, duration: 500 }}">
+        <h1 class="h2 lg:h1 mb-10" in:fly="{{ y: 20, duration: 500 }}">
           {title}
         </h1>
       {/if}
       {#if visible}
-        <h2 class="h4" in:fly="{{ y: 250, duration: 1000, delay: 500 }}">
+        <h2 class="h4 md:h3" in:fly="{{ y: 250, duration: 1000, delay: 500 }}">
           {subtitle}
         </h2>
       {/if}
     </div>
     {#if visible}
-      <img src={imageSrc} class="w-72 h-72 object-cover rounded-3xl" alt="Avatar" in:fly="{{ y: -250, duration: 1000, delay: 1250 }}"/>
+      <img src={imageSrc} class="w-48 md:w-52 lg:w-72 object-cover rounded-3xl" alt="Avatar" in:fly="{{ y: -250, duration: 1000, delay: 1250 }}"/>
     {/if}
   </div>
+
+  <div class="flex flex-wrap align-middle justify-center flex-col sm:flex-row space-x-5 sm:space-x-10 my-32">
+    {#if visible}
+      <img src={svgHello} class="w-48 md:w-72 rounded-3xl" alt="Hello" in:fly="{{ y: -250, duration: 1000, delay: 1250 }}"/>
+      <p class="flex my-5 w-full sm:w-1/2" in:fly="{{ y: -250, duration: 1000, delay: 1250 }}">
+        {about}
+      </p>
+    {/if}
+  </div>
+  
+
   <footer class="flex sticky bottom-0 justify-center">
     {#if visible}
       <a class="btn hover:variant-soft-primary flex-col down-arrow" href="/#about" style="cursor: pointer;" in:fly="{{ y: -500, duration: 1000, delay: 750 }}">

de5a3a23c0f40256fa3f7522c7d3ddf1ae2dfd1e
bliu <brianliu03@gmail.com>
2023-09-02 15:31:36 -0700

    Update formatting in hero

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -6,7 +6,7 @@
   export let subtitle = "I am a visiting student at the University of Oxford and a student at Bowdoin College.";
   export let imageSrc = "/images/hero_portrait.jpg";
   export let svgHello = "/svg/hello.svg";
-  export let about = "Hello! I am interested in utilizing technology for human good, addressing pressing humanitarian issues, and creating empowering software for all. The current housing crisis has captured my attention, and I am exploring statistical and computational models to chronicle historical events and find relationships that explain variation. I am also interested in the intersection of math, music, and creativity. This includes human computer interaction, stochastic processes, and machine learning.";
+  export let about = "Hello! I am interested in utilizing technology for human good, addressing pressing humanitarian issues, and creating empowering software for all. The current housing crisis has captured my attention, and I am exploring statistical models to chronicle historical events and find relationships that explain variation. I am also interested in the intersection of math, music, and creativity. This includes human computer interaction, stochastic processes, and machine learning.";
 
   let visible = false;
 
@@ -17,7 +17,7 @@
 </script>
 
 <div class="flex flex-col">
-  <div class="flex items-center justify-center space-x-16 mx-5 md:mx-10 md:space-x-32">
+  <div class="flex flex-col sm:flex-row items-center justify-center space-x-16 mx-5 md:mx-10 md:space-x-32">
     <div class="flex flex-col">
       {#if visible}
         <h1 class="h2 lg:h1 mb-10" in:fly="{{ y: 20, duration: 500 }}">
@@ -31,11 +31,11 @@
       {/if}
     </div>
     {#if visible}
-      <img src={imageSrc} class="w-48 md:w-52 lg:w-72 object-cover rounded-3xl" alt="Avatar" in:fly="{{ y: -250, duration: 1000, delay: 1250 }}"/>
+      <img src={imageSrc} class="w-0 sm:w-48 md:w-52 lg:w-72 object-cover rounded-3xl" alt="Avatar" in:fly="{{ y: -250, duration: 1000, delay: 1250 }}"/>
     {/if}
   </div>
 
-  <div class="flex flex-wrap align-middle justify-center flex-col sm:flex-row space-x-5 sm:space-x-10 my-32">
+  <div class="flex flex-col place-content-center place-items-center sm:flex-row space-x-5 sm:space-x-10 my-10">
     {#if visible}
       <img src={svgHello} class="w-48 md:w-72 rounded-3xl" alt="Hello" in:fly="{{ y: -250, duration: 1000, delay: 1250 }}"/>
       <p class="flex my-5 w-full sm:w-1/2" in:fly="{{ y: -250, duration: 1000, delay: 1250 }}">
@@ -43,7 +43,6 @@
       </p>
     {/if}
   </div>
-  
 
   <footer class="flex sticky bottom-0 justify-center">
     {#if visible}

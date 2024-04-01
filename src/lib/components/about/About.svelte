de91e8ab4e21636a623267f03e5dcddd81593011
bliu <brianliu03@gmail.com>
2024-04-01 08:08:40 +0100

    Update about

--- src/lib/components/about/About.svelte
+++ src/lib/components/about/About.svelte
@@ -6,7 +6,13 @@
 <AnimatedSection decoration="w-16 h-4 bg-gradient-to-r from-purple-300 to-blue-300" title="About Me" id="About">
   <div class="flex flex-col-reverse lg:flex-row">
     <p class="pt-5 h4 text-secondary-100 lg:w-7/12 lg:pr-16 lg:h3">
-      Hello! I'm a visiting student at the University of Oxford and a student at Bowdoin College. My interests include computer science, mathematics, finance, and music.
+      Hello! I'm a visiting student at the University of Oxford and a student at Bowdoin College. I like to work on software problems with a machine learning edge. I'm currently part of the
+      <a class="text-primary-500" href="https://pnpl.robots.ox.ac.uk/" target="_blank">
+        Neural Processing Lab
+      </a>, where I focus on deep learning for decoding semantic information from brain signals, and the
+      <a class="text-primary-500" href="https://ori.ox.ac.uk/labs/cognitive-robotics-group/" target="_blank">
+        Cognitive Robotics Group
+      </a>, where I work on creating a visualization framework with a robotic backpack on cyclists.
       <br /> <br />
       I am interested in utilizing technology for human good, addressing pressing humanitarian issues, and creating empowering software for all. I find fulfillment not only in the development process, but also in the human connections and communication that exist around it.
       <br /> <br />

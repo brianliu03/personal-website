b1eaf3513a0525544e5467b0f540eac80ee72737
bliu <brianliu03@gmail.com>
2024-04-22 19:03:24 +0100

    upload eviction data

---
+++ src/routes/test/+page.server.ts
@@ -0,0 +1,6 @@
+import { data } from '$lib/data/evictions.geojson';
+import type { PageServerLoad } from './$types';
+
+export const load = (async () => {
+    return {};
+}) satisfies PageServerLoad;

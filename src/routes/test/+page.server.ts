6d64f5fc5ce4767255300788d0fa94e570862170
Brian Liu <64610885+brianliu03@users.noreply.github.com>
2025-06-24 11:39:37 +0800

    Remove line with nonexistent import

--- src/routes/test/+page.server.ts
+++ src/routes/test/+page.server.ts
@@ -1,4 +1,4 @@
-import { data } from '$lib/data/evictions.geojson';
+// import { data } from '$lib/data/evictions.geojson';
 import type { PageServerLoad } from './$types';
 
 export const load = (async () => {
